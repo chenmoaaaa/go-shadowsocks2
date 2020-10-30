834d8e388bd751b79d7ee0d83088767524cb46d4
chenmoaaaa <33976355+chenmoaaaa@users.noreply.github.com>
2020-10-30 15:26:43 +0800

    Update cipher.go

--- core/cipher.go
+++ core/cipher.go
@@ -56,19 +56,23 @@ var aeadList = map[string]struct {
 }
 
 // ListCipher returns a list of available cipher names sorted alphabetically.
+//return从a~z排序的可用cipher
 func ListCipher() []string {
 	var l []string
 	for k := range aeadList {
 		l = append(l, k)
 	}
+        //sort.String按照字母顺序进行升序排序
 	sort.Strings(l)
 	return l
 }
 
 // PickCipher returns a Cipher of the given name. Derive key from password if given key is empty.
+//return给定的cipher名称，如果key为空就从password派生key
 func PickCipher(name string, key []byte, password string) (Cipher, error) {
+       //将string转换为upper(大写)
 	name = strings.ToUpper(name)
-
+       
 	switch name {
 	case "DUMMY":
 		return &dummy{}, nil
@@ -79,43 +83,53 @@ func PickCipher(name string, key []byte, password string) (Cipher, error) {
 	case "AES-256-GCM":
 		name = aeadAes256Gcm
 	}
-
+       
 	if choice, ok := aeadList[name]; ok {
+                //如果key为空就从password派生key
 		if len(key) == 0 {
 			key = kdf(password, choice.KeySize)
 		}
+                //key的大小发生错误
 		if len(key) != choice.KeySize {
 			return nil, shadowaead.KeySizeError(choice.KeySize)
 		}
+                //创建cipher
 		aead, err := choice.New(key)
 		return &aeadCipher{aead}, err
 	}
 
 	return nil, ErrCipherNotSupported
 }
-
+//aeadcipher 具体在shadowaead.Cipher下定义
 type aeadCipher struct{ shadowaead.Cipher }
-
+//TCP和UDP实现也在shadowaead下定义
 func (aead *aeadCipher) StreamConn(c net.Conn) net.Conn { return shadowaead.NewConn(c, aead) }
 func (aead *aeadCipher) PacketConn(c net.PacketConn) net.PacketConn {
 	return shadowaead.NewPacketConn(c, aead)
 }
 
 // dummy cipher does not encrypt
+//虚拟密码不需要加密
 type dummy struct{}
-
+//直接return传入的网络实现本身
 func (dummy) StreamConn(c net.Conn) net.Conn             { return c }
 func (dummy) PacketConn(c net.PacketConn) net.PacketConn { return c }
 
 // key-derivation function from original Shadowsocks
+//从原来的shadowsocks派生函数
 func kdf(password string, keyLen int) []byte {
 	var b, prev []byte
+        //return一个使用md5校验的hash接口
 	h := md5.New()
+        //防止key的长度过小
 	for len(b) < keyLen {
 		h.Write(prev)
 		h.Write([]byte(password))
+                //获取hash
 		b = h.Sum(b)
+                //确保b的长度符合大小
 		prev = b[len(b)-h.Size():]
+                //REset会清空原先输入的数据
 		h.Reset()
 	}
 	return b[:keyLen]
