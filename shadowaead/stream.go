a13ab3cd3efae80eddd86925572fda65d9b433e4
chenmoaaaa <33976355+chenmoaaaa@users.noreply.github.com>
2020-11-09 17:17:28 +0800

    Update stream.go

--- shadowaead/stream.go
+++ shadowaead/stream.go
@@ -68,6 +68,7 @@ func (w *writer) ReadFrom(r io.Reader) (n int64, err error) {
 			n += int64(nr)
                         //从buf中提取指定长度的数据
 			buf = buf[:2+w.Overhead()+nr+w.Overhead()]
+                        //从payloadBuf中提取出有效的字节数
 			payloadBuf = payloadBuf[:nr]
                         //buf[0]==byte(nr位运算右移8位)，byte==byte(nr)
 			buf[0], buf[1] = byte(nr>>8), byte(nr) // big-endian payload size//大端模式的有效载荷
@@ -77,7 +78,7 @@ func (w *writer) ReadFrom(r io.Reader) (n int64, err error) {
                         //将数据填入payloadBuf中
 			w.Seal(payloadBuf[:0], w.nonce, payloadBuf, nil)
 			increment(w.nonce)
-
+                        //将buf中的数据写入w中
 			_, ew := w.Writer.Write(buf)
 			if ew != nil {
 				err = ew
@@ -109,8 +110,9 @@ type reader struct {
 
 // NewReader wraps an io.Reader with AEAD decryption.
 //NewReader包装 io.Reader 包括Reader和AEDA decrypt
+//参数为io.Reader cipher.AEAD类型的数据，返回的是一个指针类型的type read struct
 func NewReader(r io.Reader, aead cipher.AEAD) io.Reader { return newReader(r, aead) }
-
+//参数为io.Reader cipher.AEAD类型的interface接口，返回一个指针类型的type Read struct
 func newReader(r io.Reader, aead cipher.AEAD) *reader {
 	return &reader{
 		Reader: r,
@@ -127,6 +129,7 @@ func newReader(r io.Reader, aead cipher.AEAD) *reader {
 func (r *reader) read() (int, error) {
 	// decrypt payload size
         //解密的有效载荷大小
+        //确保buf有足够的空间接受数据
 	buf := r.buf[:2+r.Overhead()]
         //将从r中读取内容填入buf中
 	_, err := io.ReadFull(r.Reader, buf)
@@ -140,16 +143,21 @@ func (r *reader) read() (int, error) {
 		return 0, err
 	}
         //设置大小
+        //<<，＆都是位运算符，<<8就是乘以2的8次方，＆是按按位与运算
+        //2位皆为1时，才为1，只要有1位不为1，那就是为0
 	size := (int(buf[0])<<8 + int(buf[1])) & payloadSizeMask
 
 	// decrypt payload
-        //解密有效载荷
+        //解密有效载荷从
+
+        //确保buf有足够的空间接受数据
 	buf = r.buf[:size+r.Overhead()]
+        //io.ReadFull(r.Reader, buf)将从io.Reader中读取的数据，存入buf中
 	_, err = io.ReadFull(r.Reader, buf)
 	if err != nil {
 		return 0, err
 	}
-
+        //将解析后的数据存入buf[:0]中
 	_, err = r.Open(buf[:0], r.nonce, buf, nil)
 	increment(r.nonce)
 	if err != nil {
@@ -161,12 +169,14 @@ func (r *reader) read() (int, error) {
 
 // Read reads from the embedded io.Reader, decrypts and writes to b.
 //从嵌入式io读取，解密并写入b在
+//接受者是指针类型，参数为[]byte，返回值是int
 func (r *reader) Read(b []byte) (int, error) {
 	// copy decrypted bytes (if any) from previous record first
         //从以前的记录在哪里复制解密的字节(如果有的话)
 	if len(r.leftover) > 0 {
                //copy()将r.leftover写入b中
 		n := copy(b, r.leftover)
+                //去除此处读取的数据
 		r.leftover = r.leftover[n:]
 		return n, nil
 	}
@@ -189,6 +199,7 @@ func (r *reader) WriteTo(w io.Writer) (n int64, err error) {
         //写入从前一个记录留下来的解密字节的
 	for len(r.leftover) > 0 {
 		nw, ew := w.Write(r.leftover)
+                //去除此处读取到的数据
 		r.leftover = r.leftover[nw:]
 		n += int64(nw)
 		if ew != nil {
@@ -216,7 +227,8 @@ func (r *reader) WriteTo(w io.Writer) (n int64, err error) {
 			}
 			break
 		}
-	}
+	
+}
 
 	return n, err
 }
@@ -238,11 +250,12 @@ type streamConn struct {
 	r *reader
 	w *writer
 }
-
+//接受者指针类型的type streamConn struct,无参数，只返回error
 func (c *streamConn) initReader() error {
        //定义随机值的大小
 	salt := make([]byte, c.SaltSize())
-       //将从网络中读取的数据存入随机值中
+        //将从网络中读取的数据存入随机值中
+        //io.ReadFull从c.Conn.中读取数据填入salt
 	if _, err := io.ReadFull(c.Conn, salt); err != nil {
 		return err
 	}
@@ -257,47 +270,54 @@ func (c *streamConn) initReader() error {
 	}
         //增加随机值
 	internal.AddSalt(salt)
-        //获取一个指针类型的reader数据，包括io.Reader cipher.AEAD interface, buf leftover byte[]
+        //获取一个指针类型的本文件中reader数据，包括io.Reader cipher.AEAD interface, buf leftover byte[]
 	c.r = newReader(c.Conn, aead)
 	return nil
 }
-
+//接受者是一个type streamConn struct，参数b []byte,返回值int
 func (c *streamConn) Read(b []byte) (int, error) {
 	if c.r == nil {
 		if err := c.initReader(); err != nil {
 			return 0, err
 		}
 	}
+        //将数据读取存入b中并返回成功读取到的字节数
 	return c.r.Read(b)
 }
-
+//接受者是一个type streamConn struct,参数是io.Writer,返回值是int64
 func (c *streamConn) WriteTo(w io.Writer) (int64, error) {
 	if c.r == nil {
 		if err := c.initReader(); err != nil {
 			return 0, err
 		}
 	}
+        //将数据写入参数w io.Writer中
 	return c.r.WriteTo(w)
 }
-
+//接受者是一个type streamConn struct，无参数，返回值只有一个error
 func (c *streamConn) initWriter() error {
+        //定义salt的大小
 	salt := make([]byte, c.SaltSize())
+        //将随机生成的数据存入salt中
 	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
 		return err
 	}
+        //获取cipher.AEAD
 	aead, err := c.Encrypter(salt)
 	if err != nil {
 		return err
 	}
+        //将数据写入数据流中
 	_, err = c.Conn.Write(salt)
 	if err != nil {
 		return err
 	}
 	internal.AddSalt(salt)
+        //或取一个本文件中的writer类型的数据
 	c.w = newWriter(c.Conn, aead)
 	return nil
 }
-
+//将数据写入streamConn w中的数据流中
 func (c *streamConn) Write(b []byte) (int, error) {
 	if c.w == nil {
 		if err := c.initWriter(); err != nil {
@@ -306,7 +326,7 @@ func (c *streamConn) Write(b []byte) (int, error) {
 	}
 	return c.w.Write(b)
 }
-
+//将读取到的数据写入w中
 func (c *streamConn) ReadFrom(r io.Reader) (int64, error) {
 	if c.w == nil {
 		if err := c.initWriter(); err != nil {
@@ -317,4 +337,5 @@ func (c *streamConn) ReadFrom(r io.Reader) (int64, error) {
 }
 
 // NewConn wraps a stream-oriented net.Conn with cipher.
+//return返回一个本文件定义的streamConn，其实现了net.Conn
 func NewConn(c net.Conn, ciph Cipher) net.Conn { return &streamConn{Conn: c, Cipher: ciph} }
